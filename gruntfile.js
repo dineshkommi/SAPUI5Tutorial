0bcfdab529640d48831e777c516d91563e6d5fc3
dineshkommi <dinesh.kommi93@gmail.com>
2018-06-04 21:04:00 +0530

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+    
+        require('load-grunt-tasks')(grunt);
+    
+        grunt.initConfig({
+            pkg : grunt.file.readJSON('package.json'),
+            connect : { 
+                serve : { // name of the target
+                    options : {
+                        hostname : "localhost",
+                        port : 8080,
+                        base : "./",
+                        open : "http://localhost:8080/webapp/index.html", 
+                        middleware : function (connect, options) {
+                            // See document in https://github.com/drewzboto/grunt-connect-proxy
+    
+                            if (!Array.isArray(options.base)) {
+                                options.base = [options.base];
+                            }
+    
+                            var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+    
+                            options.base.forEach(function (base) {
+                                middlewares.push(connect.static(base));
+                            });
+    
+                            var directory = options.directory || options.base[options.base.length - 1];
+                            middlewares.push(connect.directory(directory));
+    
+                            return middlewares;
+                        }
+                    }
+                }
+            },
+            eslint : {
+                target : ["webapp/**/*.js"]
+            }
+        });
+    
+        grunt.registerTask('serve', ['configureProxies:serve', 'connect:serve:keepalive']);
+    
+    };
