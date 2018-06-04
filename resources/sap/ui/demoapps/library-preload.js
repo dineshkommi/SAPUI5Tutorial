a2b04916d1641e6ab7af3f6d4b04be2c83e77c0f
dineshkommi <dinesh.kommi93@gmail.com>
2018-06-04 21:24:48 +0530

    anonomus function

---
+++ resources/sap/ui/demoapps/library-preload.js
@@ -0,0 +1,7 @@
+jQuery.sap.registerPreloadedModules({
+"name":"sap/ui/demoapps/library-preload",
+"version":"2.0",
+"modules":{
+	"sap/ui/demoapps/manifest.json":'{"_version":"1.9.0","sap.app":{"id":"sap.ui.demoapps","type":"library","embeds":[],"applicationVersion":{"version":"1.54.3"},"title":"SAPUI5 library containing best practice apps derived from the WebIDE templates","description":"SAPUI5 library containing best practice apps derived from the WebIDE templates","ach":"CA-UI5-FST","resources":"resources.json","offline":true},"sap.ui":{"technology":"UI5","supportedThemes":[]},"sap.ui5":{"dependencies":{"minUI5Version":"1.54","libs":{"sap.ui.core":{"minVersion":"1.54.3"},"sap.m":{"minVersion":"1.54.3"},"sap.ui.layout":{"minVersion":"1.54.3"},"sap.ushell":{"minVersion":"1.54.3"}}},"library":{"i18n":false}}}'
+}});
+//# sourceMappingURL=library-preload.js.map
